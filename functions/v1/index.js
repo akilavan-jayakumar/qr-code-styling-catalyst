15f95939ec05cb62a97090c19ce07115776de671
akilavan.j <akilavanjayakumar@gmail.com>
2024-01-23 12:54:21 +0530

    Upodated qr-code options

--- functions/v1/index.js
+++ functions/v1/index.js
@@ -12,17 +12,23 @@ app.use(express.json());
 
 app.get('/canvas', async (_req, res) => {
 	try {
+		const base64 = await fs.promises.readFile(
+			path.join(__dirname, 'logo.svg'),
+			'base64'
+		);
+
 		const options = {
 			width: 300,
 			height: 300,
 			nodeCanvas: canvas,
-			data: 'https://www.google.com/',
-			dotsOptions: {
-				color: '#000000',
-				type: 'rounded'
-			},
-			backgroundOptions: {
-				color: '#ffffff'
+			data: 'https://www.google.com',
+			image: `data:image/png;base64,${base64}`,
+			dotsOptions: { type: 'extra-rounded', color: '#000000' },
+			cornersDotOptions: { type: 'dot', color: '#000000' },
+			cornersSquareOptions: { type: 'dot', color: '#000000' },
+			imageOptions: {
+				margin: 5,
+				imageSize: 0.2
 			}
 		};
 
