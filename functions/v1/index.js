3460d576a62bfa61ed5ad4fc58d34b3b06bd2848
akilavan.j <akilavanjayakumar@gmail.com>
2024-01-23 12:33:42 +0530

    Added qr-code styling canvas

--- functions/v1/index.js
+++ functions/v1/index.js
@@ -1,24 +1,36 @@
 const os = require('os');
 const fs = require('fs');
 const path = require('path');
+const canvas = require('canvas');
 const express = require('express');
-const { createCanvas } = require('canvas');
+const {
+	QRCodeStyling
+} = require('qr-code-styling-node/lib/qr-code-styling.common');
 
 const app = express();
 app.use(express.json());
 
-app.get('/canvas', async (req, res) => {
+app.get('/canvas', async (_req, res) => {
 	try {
-		const canvas = createCanvas(200, 200);
-		const ctx = canvas.getContext('2d');
+		const options = {
+			width: 300,
+			height: 300,
+			nodeCanvas: canvas,
+			data: 'https://www.google.com/',
+			dotsOptions: {
+				color: '#000000',
+				type: 'rounded'
+			},
+			backgroundOptions: {
+				color: '#ffffff'
+			}
+		};
 
-		ctx.font = '30px Impact';
-		ctx.rotate(0.1);
-		ctx.fillText('Awesome!', 50, 100);
+		const buffer = await new QRCodeStyling(options).getRawData('png');
 
-		const outPath = path.join(os.tmpdir(), 'test.png');
+		const outPath = path.join(os.tmpdir(), 'qr.png');
 
-		await fs.promises.writeFile(outPath, canvas.toBuffer('image/png'));
+		await fs.promises.writeFile(outPath, buffer);
 		res.download(outPath);
 	} catch (err) {
 		res.status(500).send(err);
